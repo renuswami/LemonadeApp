134f2f4cbe6d2ff2d96d05cfde5f68c1a24c9ef8
renuswami <renuswami2001@gmail.com>
2024-02-05 23:10:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/renu/lemonade/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.renu.lemonade
+
+import android.os.Bundle
+import android.support.v4.os.IResultReceiver2.Default
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.renu.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LemonApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonApp() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LemonadeTheme {
+        LemonApp()
+    }
+}
