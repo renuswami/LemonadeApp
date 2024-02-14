19bd70ecf335501ec93cf6c6ee127f6ae6862da6
renuswami <renuswami2001@gmail.com>
2024-02-14 17:20:52 +0530

    Initial Commit

--- app/src/main/java/com/renu/lemonade/MainActivity.kt
+++ app/src/main/java/com/renu/lemonade/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.renu.lemonade
 
 import android.os.Bundle
-import android.support.v4.os.IResultReceiver2.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.renu.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LemonApp(     )
+                    LemonApp()
                 }
             }
         }
@@ -32,7 +50,100 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonApp() {
+    // Current step the app is displaying (remember allows the state to be retained
+    // across recompositions).
+    var currentStep by remember { mutableStateOf(1) }
 
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.lemon_tree),
+                        contentDescription = stringResource(R.string.lemon_tree_content_description),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(text = stringResource(R.string.lemon_select))
+
+                }
+            }
+            2 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.lemon_squeeze),
+                        contentDescription = stringResource(R.string.lemon_content_description),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable{
+                                currentStep = 3
+                            }
+                            .padding(8.dp)
+                            .border(
+                                BorderStroke(4.dp, Color.Black)
+
+                            )
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(text = stringResource(R.string.lemon_squeeze))
+                }
+            }
+            3 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            currentStep = 4
+                        }
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.lemon_drink),
+                        contentDescription = stringResource(R.string.lemon_content_description),
+                        modifier = Modifier.wrapContentSize()
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(text = stringResource(R.string.lemon_drink))
+                }
+            }
+            4 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.lemon_restart),
+                        contentDescription = stringResource(R.string.lemon_content_description),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 1
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(text = stringResource(R.string.lemon_restart))
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
